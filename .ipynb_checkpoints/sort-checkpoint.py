6e21f5f73543df1f321f727dbbe03b561bba9162
Sir Jason <jsiskosky@gmail.com>
2023-07-02 13:56:55 -0400

    neural net

---
+++ .ipynb_checkpoints/sort-checkpoint.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import torch
+
+# Read the text file into a pandas DataFrame
+df = pd.read_csv('sp2019.txt', delimiter=',')
+
+# separating the data
+def create_x():
+    X = []
+
+    start = [0] * 360
+
+    X.append(start)
+
+    for i in range(97740):
+        if i  % 390 == 0:
+            front = start
+        else:
+            front = X[i]
+        front = front[1:] + [df['Open'][i]]
+        front = front[1:] + [df['High'][i]]
+        front = front[1:] + [df['Low'][i]]
+        front = front[1:] + [df['Close'][i]]
+        # front = front[1:] + [df['Open'][0]]
+        X.append(front)
+        # if i % 390 == 0:
+            
+    X.pop(0)
+    return X
+
+def create_y(X):
+    for i in range(1): # make the Y variable list
+        if df['Close'][i+10] > X[i][359]:
+            return
+
+X = create_x()
+
+Y = create_y(X)
--- sort.py
+++ sort.py
@@ -4,42 +4,38 @@ import torch
 # Read the text file into a pandas DataFrame
 df = pd.read_csv('sp2019.txt', delimiter=',')
 
-col = df['Minute'] # every 390 indices it starts another day
-
 # separating the data
-X = []
+def create_x():
+    X = []
 
-start = [0] * 400
+    start = [0] * 360
 
-X.append(start)
+    X.append(start)
 
-for i in range(97740):
-    if i  % 390 == 0:
-        front = start
-    else:
-        front = X[i]
-    front = front[1:] + [df['Open'][i]]
-    front = front[1:] + [df['High'][i]]
-    front = front[1:] + [df['Low'][i]]
-    front = front[1:] + [df['Close'][i]]
-    # front = front[1:] + [df['Open'][0]]
-    X.append(front)
-    # if i % 390 == 0:
-        
-X.pop(0)
+    for i in range(97740):
+        if i  % 390 == 0:
+            front = start
+        else:
+            front = X[i]
+        front = front[1:] + [df['Open'][i]]
+        front = front[1:] + [df['High'][i]]
+        front = front[1:] + [df['Low'][i]]
+        front = front[1:] + [df['Close'][i]]
+        # front = front[1:] + [df['Open'][0]]
+        X.append(front)
+        # if i % 390 == 0:
+            
+    X.pop(0)
+    return X
 
-# test
-t = 97739
-print(X[t])
-print(X[t][396:400])
+def create_y(X):
+    Y = []
+    for i in range(1): # make the Y variable list
+        if df['Close'][i+10] > X[i][359]:
+            Y.append(1)
+        else:
+            Y.append(0)
 
-print([df['Open'][t], df['High'][t], df['Low'][t], df['Close'][t]])
+X = create_x()
 
-
-
-# Open the file in write mode
-with open('usable_x.txt', 'w') as file:
-    # Iterate over the list and write each element to a new line
-    for item in X:
-        file.write(str(item))
-        file.write('/n')
+Y = create_y(X)
